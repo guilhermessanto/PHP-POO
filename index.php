a5494b66fcd6f9615857a4bf0bd244aa2206af07
Guilherme Santana <g.santossantana22@gmail.com>
2022-06-07 08:09:31 -0300

    atualização

--- index.php
+++ index.php
@@ -25,13 +25,14 @@ $clienteA = new Cliente();
 $clienteA ->nome = "Guilherme";
 $clienteA ->email = "g.santossantana22@gmail.com";
 $clienteA ->senha = "1234567";
-$clienteA ->telefones = ['11976756555','11981229731'];
+$clienteA ->telefones = [11976756555,11981229731];
 
 $clienteB = new Cliente();
 $clienteB ->nome = "Carol";
 $clienteB ->email = "carol@gmail.com";
 $clienteB ->senha = "12345678";
-$clienteB ->telefones = ['112345678','1140028922'];
+$clienteB ->telefones = [112345678,1140028922];
+
 
 ?>
 <pre><?= var_dump($clienteA,$clienteB)?></pre>
--- src/cliente.php
+++ src/cliente.php
@@ -7,14 +7,3 @@ class Cliente{
     public array $telefones;
 }
 
-// $clienteA = new Cliente();
-// $clienteA ->nome = "Guilherme";
-// $clienteA ->email = "g.santossantana22@gmail.com";
-// $clienteA ->senha = "1234567";
-// $clienteA ->telefones = ['11976756555','11981229731'];
-
-$clienteB = new Cliente();
-$clienteB ->nome = "Carol";
-$clienteB ->email = "carol@gmail.com";
-$clienteB ->senha = "12345678";
-$clienteB ->telefones = ['112345678','1140028922'];
