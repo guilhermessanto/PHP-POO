8b538d9904499c8c0678dd6ca7d40ecb7164b318
Guilherme Santana <g.santossantana22@gmail.com>
2022-06-08 08:55:06 -0300

    Exemplo 3

--- index.php
+++ index.php
@@ -4,28 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2</title>
+    <title>Exemplo 3</title>
 </head>
 <body>
-    <h1>PHP POO - Exemplo 2</h1>
+    <h1>PHP POO - Exemplo 3</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação dos objetos</li>
-        <li>Uso do método construtor</li>
-        <li>Uso do <code>$this</code> para acessar as
-        propriedades</li>
+        <li>Acesso direto à propriedades</li>
+        <li>Atribuição de dados e leitura</li>
     </ul>
 <?php
 // Importando a classe
 require_once "src/Cliente.php";
 
 // Criação dos objetos
-$clienteA = new Cliente('Tiago', 'tiago@gmail.com');
-$clienteB = new Cliente('Jon Oliva', 'savatage@msn.com');
+$clienteA = new Cliente;
+$clienteB = new Cliente;
+
+//Atribuindo dados às propriedades do objeto
+$clienteA->nome = "Guilherme";
+$clienteA->email = "guilherme@gmail.com";
+$clienteA->telefones = ["11-2345-6789" , "11-99989-9999"];
+
+$clienteB->nome = "Jean";
+$clienteB->email = "jean@gmail.com";
+$clienteB->telefones = array("11-98995-2154")
 ?>
 
-<pre> <?=var_dump($clienteA, $clienteB)?> </pre>
+<h2>Dados dos objetos (leitura)</h2>
+<h3><?= $clienteA->nome ?></h3>
+<p>E-mail: <?=$clienteA->email?></p>
+<p>Telefones: <?=implode(", ", $clienteA->telefones)?></p>
+<p>Senha: <?=$clienteA->senha?></p>
+
+
+<h3><?= $clienteB->nome ?></h3>
+<p>E-mail: <?=$clienteB->email?></p>
+<p>Telefones: <?=implode(", ", $clienteA->telefones)?></p>
+<p>Senha: <?=$clienteB->senha?></p>
 
 </body>
 </html>
--- src/cliente.php
+++ src/cliente.php
@@ -3,7 +3,7 @@ class Cliente {
     // Propriedades (ou atributos)
     public string $nome;
     public string $email;
-    public string $senha;
+    public string $senha = "";
     public array $telefones;
 
 }
