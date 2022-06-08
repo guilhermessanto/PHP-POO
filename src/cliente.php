1386e4b6446016c0700896d192572246db6b6ec1
Guilherme Santana <g.santossantana22@gmail.com>
2022-06-08 08:50:14 -0300

    atualização exemplo 3

--- src/cliente.php
+++ src/cliente.php
@@ -6,14 +6,4 @@ class Cliente {
     public string $senha;
     public array $telefones;
 
-    // Método construtor
-    public function __construct( string $nome, string $email )
-    {
-        /* $this é uma variável especial, sempre
-        apontando para a referência da Classe atualmente
-        manipulada. */
-        $this->nome = $nome;
-        $this->email = $email;
-    }
-
 }
